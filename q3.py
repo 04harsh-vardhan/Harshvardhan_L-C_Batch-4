a62c104691587c6cba3abe393e3c7808d9469ce6
04harsh-vardhan <harshvardhan.s@intimetec.com>
2024-12-14 11:22:19 +0530

    Assignment-1

---
+++ q3.py
@@ -0,0 +1,29 @@
+def getArmstrongNumber(userInput):
+    # Initializing Sum and Number of Digits
+    armstrongNumber = 0
+    totalDigitCountInNumber = 0
+
+    # Calculating Number of individual digits
+    userInputCopy = userInput
+    while userInputCopy > 0:
+        totalDigitCountInNumber = totalDigitCountInNumber + 1
+        userInputCopy = userInputCopy // 10
+
+    # Finding Armstrong Number
+    userInputCopy = userInput
+    for n in range(1, userInputCopy + 1):
+        unitPlaceDigit = userInputCopy % 10
+        armstrongNumber = armstrongNumber + (unitPlaceDigit ** totalDigitCountInNumber)
+        userInputCopy //= 10
+    return armstrongNumber
+
+
+# End of Function
+
+# User Input
+userInput = int(input("\nPlease Enter the Number to Check for Armstrong: "))
+
+if (userInput == getArmstrongNumber(userInput)):
+    print("\n %d is Armstrong Number.\n" % userInput)
+else:
+    print("\n %d is Not a Armstrong Number.\n" % userInput)
